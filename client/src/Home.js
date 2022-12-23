09384dab1bbabc00c5bf82f6ce161c1e7366e72f
Tvya <tvya1995@gmail.com>
2022-12-24 01:11:53 +0800

    Add Homepage

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,21 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import NavBar from './NavBar';
-
+import {Link,useNavigate,useLocation} from 'react-router-dom';
 
 function Home() {
 
 
   return (
-    <div className="Home">
-      <header className="Home-header"> 
-    
-       <div>
-        <h1></h1>
-       </div>
-
-      </header>
-    </div>
+    <div>
+      <div className="Home-Title">
+        <h1>Skin Mixology</h1>
+      </div>
+     <div className = "Title-1">
+          <p>A place for you to curate your own skincare regime base on your skin goal , using various products , across various skincare brands </p><br></br>
+          <p>But the most important step to building your skincare is to know you skintype. <Link to="/HowToKnowYourSkinType">Find out your skintype</Link></p><br></br>         
+   </div>
+   </div>
   );
 }
 
